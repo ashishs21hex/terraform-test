0fff9509f46549101378c90b14b06855f5758466
ashishs21hex <ashishs21@hexaware.com>
2025-02-27 06:56:03 +0000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,6 +4,10 @@ provider "aws" {
 
 resource "aws_s3_bucket" "example" {
   bucket = "example-bucket-${random_pet.name}"
+}
+
+resource "aws_s3_bucket_acl" "example_acl" {
+  bucket = aws_s3_bucket.example.id
   acl    = "private"
 }
 
